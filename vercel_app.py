84953c8d63210795bc77783ee81fa2366edc524a
Harshit <128028183+harshit141222@users.noreply.github.com>
2024-12-16 02:30:41 +0530

    Add files via upload

--- vercel_app.py
+++ vercel_app.py
@@ -1,4 +1,17 @@
-from quiz_project.wsgi import application
+import os
+from django.core.wsgi import get_wsgi_application
+from django.core.management import execute_from_command_line
 
-# Init the WSGI app
+# Set up Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiz_project.settings')
+
+# Run migrations and collect static files
+if os.environ.get('VERCEL_ENV') == 'production':
+    execute_from_command_line(['manage.py', 'collectstatic', '--noinput'])
+    execute_from_command_line(['manage.py', 'migrate'])
+
+# Initialize Django WSGI application
+application = get_wsgi_application()
+
+# Export the WSGI application
 app = application
